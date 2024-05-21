b3ec95bc0b6ee2769dbdd9523a91651a10c0eaa1
Akash Prabhu <akash.prabhu@moonraft.com>
2024-05-21 18:04:14 +0530

    Header changes

---
+++ blocks/header-data-new/header-data-new.js
@@ -0,0 +1,10 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
+
+/**
+ * loads and decorates the footer
+ * @param {Element} block The footer block element
+ */
+export default async function decorate(block) {
+    console.log('block: ', block);
+}
