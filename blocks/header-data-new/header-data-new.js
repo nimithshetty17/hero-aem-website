90cfe727affb46c5fdb08cd5f13b46cdab9fa5e8
Akash Prabhu <akash.prabhu@moonraft.com>
2024-05-21 18:22:34 +0530

    Changes

--- blocks/header-data-new/header-data-new.js
+++ blocks/header-data-new/header-data-new.js
@@ -1,20 +1,8 @@
-import { getMetadata } from '../../scripts/aem.js';
-import { loadFragment } from '../fragment/fragment.js';
-
-/**
- * loads and decorates the footer
- * @param {Element} block The footer block element
- */
 export default async function decorate(block) {
-    const fragment = await loadFragment('/header-data-new');
-
-    block.textContent = '';
-    const newHeader = document.createElement('div');
-    while (fragment.firstElementChild) newHeader.append(fragment.firstElementChild);
-    console.log('newHeader: ', newHeader);
-
-    const headerTabs = newHeader.querySelector('.header-tabs');
+    const headerTabs = document.querySelector('.header-tabs');
     console.log('headerTabs: ', headerTabs);
 
-    block.append(newHeader);
+    const aTags = document.querySelectorAll('a');
+    console.log('a tags ', aTags);
+
 }
